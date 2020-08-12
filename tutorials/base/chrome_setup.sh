9d4170bacaa84cef0177812ac8ed356bc481da8f
artem <artem.borodavka@applitools.com>
2020-08-12 15:39:13 +0300

    update base image for the tutorials

--- tutorials/base/chrome_setup.sh
+++ tutorials/base/chrome_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-apt-get install google-chrome-stable
+needUpdate=$(apt-get -s upgrade | grep google-chrome-stable)
+if [ "$needUpdate" -eq 0 ]
+then
+  echo 'Latest version of the google-chrome is already used'
+else
+  apt-get install google-chrome-stable
+  bash ./opt/bin/wrap_chrome_binary
+fi
 wget - "https://chromedriver.storage.googleapis.com/$(wget -qO - "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")/chromedriver_linux64.zip"
 unzip chromedriver_linux64
 mv chromedriver /usr/bin/
