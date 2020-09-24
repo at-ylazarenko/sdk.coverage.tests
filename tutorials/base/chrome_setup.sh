ddc913e36ff5cf85be5218d0f4dad92f87d17ae3
artem <artem.borodavka@applitools.com>
2020-09-24 12:13:58 +0300

    update Dockerfile

--- tutorials/base/chrome_setup.sh
+++ tutorials/base/chrome_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-needUpdate=$(apt-get -s upgrade | grep google-chrome-stable)
+apt-get -s upgrade | grep -q google-chrome-stable
+needUpdate=$?
 if [ "$needUpdate" -eq 0 ]
 then
-  echo 'Latest version of the google-chrome is already used'
-else
   apt-get update
   apt-get install google-chrome-stable
   bash ./opt/bin/wrap_chrome_binary
+else
+  echo 'Latest version of the google-chrome is already used'
 fi
 wget - "https://chromedriver.storage.googleapis.com/$(wget -qO - "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")/chromedriver_linux64.zip"
 unzip chromedriver_linux64
